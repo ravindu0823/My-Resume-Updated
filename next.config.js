46c5e96d499d0a54180880fc39c478ba4a53cb83
Ravindu Perera <guestpc87@gmail.com>
2024-09-28 18:03:51 +0530

    Refactor image domains in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,18 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['res.cloudinary.com', 'firebasestorage.googleapis.com', 'img.icons8.com', 'raw.githubusercontent.com', 'i.imgur.com', 'img.freepik.com','media.geeksforgeeks.org']
-  }
-}
+    domains: [
+      "res.cloudinary.com",
+      "firebasestorage.googleapis.com",
+      "img.icons8.com",
+      "raw.githubusercontent.com",
+      "i.imgur.com",
+      "img.freepik.com",
+      "media.geeksforgeeks.org",
+      "i.ibb.co",
+      "camo.githubusercontent.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
