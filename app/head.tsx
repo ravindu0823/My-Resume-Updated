f5ac9d022abfe33500affe010bb301997628ce0f
Ravindu Perera <guestpc87@gmail.com>
2024-09-28 18:04:34 +0530

    Refactor meta tags in app/head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,23 +2,49 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta name="keywords" content="jigar sable, portfolio, jigar, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
-      <meta property="og:site_name" content="Jigar Sable's Portfolio" />
-      <meta property="og:title" content="Portfolio | Jigar Sable - Full Stack Developer" />
-      <meta property="og:description"
-        content="I'm Jigar Sable, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta property="og:url" content="https://jigarsable.vercel.app" />
-      <meta property="og:image" content="https://jigarsable.vercel.app/portfolio-fork.png" />
-      <link rel="canonical" href="https://jigarsable.vercel.app" />
-      <meta name="author" content="Jigar Sable" />
+      <meta
+        name="description"
+        content="I'm Ravindu Perera, a full-stack developer passionate about creating and designing products that simplify daily life. With over 3 years of experience in software engineering, I focus on delivering innovative solutions."
+      />
+      <meta
+        name="keywords"
+        content="ravindu perera, portfolio, ravindu, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio"
+      />
+      <meta property="og:site_name" content="Ravindu Perera's Portfolio" />
+      <meta
+        property="og:title"
+        content="Portfolio | Ravindu Perera - Full Stack Developer"
+      />
+      <meta
+        property="og:description"
+        content="I'm Ravindu Perera, an enthusiastic tech lover with over 3 years of experience in software engineering. I specialize in full-stack development and research, creating innovative products that make life easier."
+      />
+      <meta
+        property="og:url"
+        content="https://your-portfolio-link.vercel.app"
+      />
+      <meta
+        property="og:image"
+        content="https://your-portfolio-link.vercel.app/portfolio-image.png"
+      />
+      <link rel="canonical" href="https://your-portfolio-link.vercel.app" />
+      <meta name="author" content="Ravindu Perera" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://jigarsable.vercel.app" />
-      <meta name="application-name" content="Portfolio | Jigar Sable - Full Stack Developer" />
-      <meta name="apple-mobile-web-app-title" content="Portfolio | Jigar Sable - Full Stack Developer" />
+      <meta
+        name="twitter:site"
+        content="https://your-portfolio-link.vercel.app"
+      />
+      <meta
+        name="application-name"
+        content="Portfolio | Ravindu Perera - Full Stack Developer"
+      />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="Portfolio | Ravindu Perera - Full Stack Developer"
+      />
       <link rel="icon" href="/favicon.ico" />
-      <title>Portfolio | Jigar Sable - Full Stack Developer</title>
+      <title>Portfolio | Ravindu Perera - Full Stack Developer</title>
     </>
-  )
+  );
 }
